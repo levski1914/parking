70d8e557849c97798fa7ee28e2052b96cbd83769
levski1914 <georgi.dimitrov@abv.bg>
2026-05-04 21:12:31 +0300

    sup

--- parkbg-api/src/main.ts
+++ parkbg-api/src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://parking-j0e2m98zl-lamborghinigd-4806s-projects.vercel.app',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(
